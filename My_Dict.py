996b8fef70a92eb03ac715e84640112cdf919e40
Hrithik Bandil <47934941+Bandils0606@users.noreply.github.com>
2019-05-22 12:29:21 +0530

    Program for creating a Dictionary

---
+++ My_Dict.py
@@ -0,0 +1,4 @@
+dic={'Audi':'https://en.wikipedia.org/wiki/Audi', 'Mercedes':'https://en.wikipedia.org/wiki/Mercedes',
+     'BMW':'https://en.wikipedia.org/wiki/BMW', 'Jaguar':'https://en.wikipedia.org/wiki/Jaguar_Cars'}
+a=dic[input('Enter the name of the set:')]
+print(a)
