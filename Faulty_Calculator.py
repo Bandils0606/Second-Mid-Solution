d996dc3258f63930f2c8e06b807cc9add6fedf6e
Hrithik Bandil <47934941+Bandils0606@users.noreply.github.com>
2019-05-25 18:51:47 +0530

    Update Faulty_Calculator.py

--- Faulty_Calculator.py
+++ Faulty_Calculator.py
@@ -2,38 +2,38 @@
 # The program must return following faulty values:-
 # 56+9=77; 45*3=555 and 56/6=4
 
-a=int(input('Enter a number:'))
-b=int(input('Enter another number:'))
-print(
-    '''
-    Operations:
-      --> +
-      --> -
-      --> *
-      --> /
-      '''
-     )
+cont = 'y'
+while True:
+    if cont == 'n':
+        break
+    a=int(input('Enter a number:'))
+    b=int(input('Enter another number:'))
+    print(
+        '''
+        Operations:
+        --> +
+        --> -
+        --> *
+        --> /
+        '''
+        )
 
-c=input('Select your choice:')
+    c=input('Select your choice:')
 
-if a==56  and b==9  and c=='+':
-    print(77)
-elif a==45  and b==3  and c=='*':
-    print(555)
-elif a==56  and b==6  and c=='/':
-    print(4)
-elif c=='+' and a!=56 and b!=9:
-    print(a+b)
-elif c=='-':
-    print(a-b)
-elif c=='*' and a!=45 and b!=3:
-    print(a*b)
-elif c=='/' and a!=56 and b!=6:
-    print(a/b)
-else:
-    print('Invalid Choice.')
-
-
-
-
-
+    if a==56  and b==9  and c=='+':
+        print(77)
+    elif a==45  and b==3  and c=='*':
+        print(555)
+    elif a==56  and b==6  and c=='/':
+        print(4)
+    elif c=='+':
+        print(a+b)
+    elif c=='-':
+        print(a-b)
+    elif c=='*':
+        print(a*b)
+    elif c=='/':
+        print(a/b)
+    else:
+        print('Invalid Choice.')
+    cont=input('Do you want to Continue? Enter\'n\' for no and \'y\' for yes:')
